9faeafcd60371eb96ed0f7250264b3cd0cf50088
justin <shieh_justin@yahoo.com>
2020-01-09 18:02:16 -0800

    problem 1

---
+++ min_number_of_increments_to_make_all_array_equal.js
@@ -0,0 +1,13 @@
+// We are given an array consisting of n elements. At each operation you can select any one 
+// element and increase rest of n-1 elements by 1. You have to make all elements equal performing 
+// such operation as many times you wish. Find the minimum number of operations needed for this.
+
+// Brute force
+// A simple way to make all elements equal is that at each step find the largest elements and then 
+// increase all rest n-1 elements by 1. We should keep doing this operation till all elements become equal. 
+// Time Complexity : O(n^2)
+
+// Optimized 
+
+// Decrement all elements to the smallest element
+// minOp = arrSum - n * smallest
