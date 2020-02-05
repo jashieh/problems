efcb3910dbc6f1b4b433dec78522342412f1ba99
justin <shieh_justin@yahoo.com>
2020-02-04 22:38:32 -0800

    find peak ele

---
+++ leetcode_interview_medium/find_peak_element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    
+    while(low < high) {
+        let mid = Math.floor((high+low)/2);
+        
+        if (nums[mid] > nums[mid+1]) {
+            high = mid;            
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return low;
+};
