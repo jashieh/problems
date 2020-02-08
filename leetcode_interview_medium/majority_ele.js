5665085ea149a355c0527208fe2271bc4533409d
justin <shieh_justin@yahoo.com>
2020-02-07 20:02:49 -0800

    majority

---
+++ leetcode_interview_medium/majority_ele.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let ans = 0;
+    let count = 0;
+    
+    for(let num of nums) {
+        if(count === 0) ans = num;
+        count += num === ans ? 1 : -1;
+    }
+    return ans;
+};
