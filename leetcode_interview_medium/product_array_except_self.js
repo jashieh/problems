9c0156227946ea060e2005daf981e018437f9811
justin <shieh_justin@yahoo.com>
2020-02-09 20:50:19 -0800

    sol 2

--- leetcode_interview_medium/product_array_except_self.js
+++ leetcode_interview_medium/product_array_except_self.js
@@ -25,3 +25,22 @@ var productExceptSelf = function(nums) {
     
     return ans;
 };
+
+//
+var productExceptSelf = function(nums) {
+    let ans = new Array(nums.length);
+    
+    
+    ans[0] = 1;
+    for(let i = 1; i < nums.length; i++) {
+        ans[i] = ans[i-1] * nums[i-1];
+    }
+    
+    let r = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        ans[i] = ans[i] * r;
+        r *= nums[i];
+    }
+        
+    return ans;
+};
