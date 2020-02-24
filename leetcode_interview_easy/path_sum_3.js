07f8b90032f15e59979b2ad3b84dc4ef04c56502
justin <shieh_justin@yahoo.com>
2020-02-23 23:16:05 -0800

    optimized

--- leetcode_interview_easy/path_sum_3.js
+++ leetcode_interview_easy/path_sum_3.js
@@ -26,3 +26,38 @@ function subPath(root, sum) {
     
     return count;
 }
+
+//
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function(root, sum) {
+    if(!root) return 0;
+    let memo = {};
+    memo[0] = 1;
+    return subPath(root,0,sum,memo);
+    
+};
+
+function subPath(root, sum, target, memo) {
+    if(!root) return 0;
+    sum += root.val;
+
+    let count = sum - target in memo ? memo[sum-target] : 0;
+    
+    memo[sum] = sum in memo ? memo[sum] + 1 : 1;
+    
+    let res = count + subPath(root.left, sum, target, memo) + subPath(root.right,sum,target,memo);
+    
+    memo[sum] = memo[sum] - 1;
+    return res;
+}
