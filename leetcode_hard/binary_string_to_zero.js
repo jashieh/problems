271b5096a0ae6e8713a6b18ccb5e7341305410af
justin <shieh_justin@yahoo.com>
2020-03-04 17:32:14 -0800

    brute force

--- leetcode_hard/binary_string_to_zero.js
+++ leetcode_hard/binary_string_to_zero.js
@@ -4,8 +4,24 @@ function steps(str, k) {
     for(let i = 0; i < str.length; i++) {
         ans += "0";
     }
-    console.log(ans);
-    // while(str !=)
+
+    let count = 0;
+
+    while(str != ans) {
+        let x = str.indexOf("1");
+        while(x < l) {
+            if(str[x] === "1") {
+                str = str.slice(0,x) + "0" + str.slice(x+1);
+            } else {
+                str = str.slice(0,x) + "1" + str.slice(x+1);
+            }
+            x += k;
+        }
+        console.log(str);
+        count++;
+    }
+
+    return count;
 }
 
-steps("100010010011110", 2);
+console.log(steps("100010010011110", 2));
