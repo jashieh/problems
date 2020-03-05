5099fbbf880913bb2ee4678cbd34b01de40710a1
justin <shieh_justin@yahoo.com>
2020-03-04 17:22:29 -0800

    bin to string

---
+++ leetcode_hard/binary_string_to_zero.js
@@ -0,0 +1,11 @@
+function steps(str, k) {
+    let l = str.length;
+    let ans = "";
+    for(let i = 0; i < str.length; i++) {
+        ans += "0";
+    }
+    console.log(ans);
+    // while(str !=)
+}
+
+steps("100010010011110", 2);
